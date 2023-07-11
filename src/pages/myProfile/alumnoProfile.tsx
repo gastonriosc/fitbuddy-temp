0b2d3e0514ef5e25fd8cfb42f8dc4674885c39be
Joaquin Manero <joaquinmanero@Joaquins-MacBook-Pro.local>
2023-07-10 21:41:47 -0300

    visualizacion de perfiles de alumno y entrenador, y creacion de tabla de planes

--- src/pages/myProfile/alumnoProfile.tsx
+++ src/pages/myProfile/alumnoProfile.tsx
@@ -1,18 +1,363 @@
 
 // ** MUI Imports
+// ** MUI Imports
+import Box from '@mui/material/Box'
 import Grid from '@mui/material/Grid'
 import Card from '@mui/material/Card'
+import Select from '@mui/material/Select'
+import Dialog from '@mui/material/Dialog'
+import Divider from '@mui/material/Divider'
+import { styled } from '@mui/material/styles'
+import Checkbox from '@mui/material/Checkbox'
+import MenuItem from '@mui/material/MenuItem'
+import TextField from '@mui/material/TextField'
+import Typography from '@mui/material/Typography'
+import InputLabel from '@mui/material/InputLabel'
 import CardHeader from '@mui/material/CardHeader'
+import FormControl from '@mui/material/FormControl'
 import CardContent from '@mui/material/CardContent'
+import DialogContent from '@mui/material/DialogContent'
+import DialogActions from '@mui/material/DialogActions'
+import FormHelperText from '@mui/material/FormHelperText'
+import InputAdornment from '@mui/material/InputAdornment'
+import Button, { ButtonProps } from '@mui/material/Button'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import { ChangeEvent, ElementType, useState } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import Icon from 'src/@core/components/icon'
+
+
+interface Data {
+  email: string
+  country: string
+  lastName: string
+  genre: string
+  firstName: string
+  number: number | string
+  password: string
+}
+
+const initialData: Data = {
+  number: '',
+  password: '',
+  lastName: 'Tissera',
+  firstName: 'Facundo',
+  genre: '',
+  country: 'argentina',
+  email: 'facutissera@example.com',
+}
+
+const ImgStyled = styled('img')(({ theme }) => ({
+  width: 120,
+  height: 120,
+  marginRight: theme.spacing(5),
+  borderRadius: theme.shape.borderRadius
+}))
+
+const ButtonStyled = styled(Button)<ButtonProps & { component?: ElementType; htmlFor?: string }>(({ theme }) => ({
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    textAlign: 'center'
+  }
+}))
+
+const ResetButtonStyled = styled(Button)<ButtonProps>(({ theme }) => ({
+  marginLeft: theme.spacing(4),
+  [theme.breakpoints.down('sm')]: {
+    width: '100%',
+    marginLeft: 0,
+    textAlign: 'center',
+    marginTop: theme.spacing(4)
+  }
+}))
+
 
 const AlumnoProfile = () => {
+  const [open, setOpen] = useState<boolean>(false)
+  const [inputValue, setInputValue] = useState<string>('')
+  const [userInput, setUserInput] = useState<string>('yes')
+  const [formData, setFormData] = useState<Data>(initialData)
+  const [imgSrc, setImgSrc] = useState<string>('/images/avatars/1.png')
+  const [secondDialogOpen, setSecondDialogOpen] = useState<boolean>(false)
+
+  // ** Hooks
+  const {
+    control,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({ defaultValues: { checkbox: false } })
+
+  const handleClose = () => setOpen(false)
+
+  const handleSecondDialogClose = () => setSecondDialogOpen(false)
+
+  const handleFormChange = (field: keyof Data, value: Data[keyof Data]) => {
+    setFormData({ ...formData, [field]: value })
+  }
+
+  const onSubmit = () => setOpen(true)
+
+  const handleConfirmation = (value: string) => {
+    handleClose()
+    setUserInput(value)
+    setSecondDialogOpen(true)
+  }
+
+  const handleInputImageChange = (file: ChangeEvent) => {
+    const reader = new FileReader()
+    const { files } = file.target as HTMLInputElement
+    if (files && files.length !== 0) {
+      reader.onload = () => setImgSrc(reader.result as string)
+      reader.readAsDataURL(files[0])
+
+      if (reader.result !== null) {
+        setInputValue(reader.result as string)
+      }
+    }
+  }
+  const handleInputImageReset = () => {
+    setInputValue('')
+    setImgSrc('/images/avatars/1.png')
+  }
+
+
   return (
-    <Grid item md={6} xs={12}>
-      <Card>
-        <CardHeader title='Mi perfil Alumno' />
-        <CardContent>
-        </CardContent>
-      </Card>
+    <Grid container spacing={6}>
+
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title={`Alumno: ${formData.firstName} ${formData.lastName}`} />
+          <form >
+            <CardContent sx={{ pt: 0 }}>
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <ImgStyled src={imgSrc} alt='Profile Pic' />
+                <div>
+                  <ButtonStyled component='label' variant='contained' htmlFor='account-settings-upload-image'>
+                    Subir foto
+                    <input
+                      hidden
+                      type='file'
+                      value={inputValue}
+                      accept='image/png, image/jpeg'
+                      onChange={handleInputImageChange}
+                      id='account-settings-upload-image'
+                    />
+                  </ButtonStyled>
+                  <ResetButtonStyled color='secondary' variant='outlined' onClick={handleInputImageReset}>
+                    Volver al estado inicial
+                  </ResetButtonStyled>
+                  <Typography sx={{ mt: 5, color: 'text.disabled' }}>Formato PNG o JPEG. Tamaño máximo de 800K.</Typography>
+                </div>
+              </Box>
+            </CardContent>
+            <Divider />
+            <CardContent>
+              <Grid container spacing={6}>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    label='Nombre Completo'
+                    placeholder='Facundo Tissera'
+                    value={formData.firstName}
+                    onChange={e => handleFormChange('firstName', e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    type='email'
+                    label='Email'
+                    value={formData.email}
+                    placeholder='facutissera@example.com'
+                    onChange={e => handleFormChange('email', e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    type='password'
+                    label='Contraseña'
+                    value={formData.password}
+                    onChange={e => handleFormChange('password', e.target.value)}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <TextField
+                    fullWidth
+                    type='number'
+                    label='Teléfono'
+                    value={formData.number}
+                    placeholder='3513452255'
+                    onChange={e => handleFormChange('number', e.target.value)}
+                    InputProps={{ startAdornment: <InputAdornment position='start'>ARG (+54)</InputAdornment> }}
+                  />
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth>
+                    <InputLabel>País</InputLabel>
+                    <Select
+                      label='País'
+                      value={formData.country}
+                      onChange={e => handleFormChange('country', e.target.value)}
+                    >
+                      <MenuItem value='argentina'>Argentina</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+                <Grid item xs={12} sm={6}>
+                  <FormControl fullWidth>
+                    <InputLabel>Género</InputLabel>
+                    <Select
+                      label='Genero'
+                      value={formData.genre}
+                      onChange={e => handleFormChange('genre', e.target.value)}
+                    >
+                      <MenuItem value='femenino'>Femenino</MenuItem>
+                      <MenuItem value='masculino'>Masculino</MenuItem>
+                      <MenuItem value='otro'>Otro</MenuItem>
+                    </Select>
+                  </FormControl>
+                </Grid>
+
+
+                <Grid item xs={12}>
+                  <Button variant='contained' sx={{ mr: 3 }}>
+                    Guardar cambios
+                  </Button>
+                  <Button type='reset' variant='outlined' color='secondary' onClick={() => setFormData(initialData)}>
+                    Reestablecer
+                  </Button>
+                </Grid>
+              </Grid>
+            </CardContent>
+          </form>
+        </Card>
+      </Grid >
+
+      {/* Delete Account Card */}
+      <Grid item xs={12}>
+        <Card>
+          <CardHeader title='Eliminar cuenta' />
+          <CardContent>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <Box sx={{ mb: 4 }}>
+                <FormControl>
+                  <Controller
+                    name='checkbox'
+                    control={control}
+                    rules={{ required: true }}
+                    render={({ field }) => (
+                      <FormControlLabel
+                        label='Soy consciente de que quiero borrar la cuenta'
+                        sx={errors.checkbox ? { '& .MuiTypography-root': { color: 'error.main' } } : null}
+                        control={
+                          <Checkbox
+                            {...field}
+                            size='small'
+                            name='validation-basic-checkbox'
+                            sx={errors.checkbox ? { color: 'error.main' } : null}
+                          />
+                        }
+                      />
+                    )}
+                  />
+                  {errors.checkbox && (
+                    <FormHelperText sx={{ color: 'error.main' }} id='validation-basic-checkbox'>
+                      Por favor, confirme de que quiere eliminar la cuenta.
+                    </FormHelperText>
+                  )}
+                </FormControl>
+              </Box>
+              <Button variant='contained' color='error' type='submit' disabled={errors.checkbox !== undefined}>
+                Eliminar
+              </Button>
+            </form>
+          </CardContent>
+        </Card>
+      </Grid>
+
+      {/* Deactivate Account Dialogs */}
+      <Dialog fullWidth maxWidth='xs' open={open} onClose={handleClose}>
+        <DialogContent
+          sx={{
+            pb: theme => `${theme.spacing(6)} !important`,
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pt: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              textAlign: 'center',
+              alignItems: 'center',
+              flexDirection: 'column',
+              justifyContent: 'center',
+              '& svg': { mb: 6, color: 'warning.main' }
+            }}
+          >
+            <Icon icon='mdi:alert-circle-outline' fontSize='5.5rem' />
+            <Typography>Está seguro de eliminar su cuenta?</Typography>
+          </Box>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            justifyContent: 'center',
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        >
+          <Button variant='contained' sx={{ mr: 2 }} onClick={() => handleConfirmation('yes')}>
+            Sí
+          </Button>
+          <Button variant='outlined' color='secondary' onClick={() => handleConfirmation('cancel')}>
+            No
+          </Button>
+        </DialogActions>
+      </Dialog>
+      <Dialog fullWidth maxWidth='xs' open={secondDialogOpen} onClose={handleSecondDialogClose}>
+        <DialogContent
+          sx={{
+            pb: theme => `${theme.spacing(6)} !important`,
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pt: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        >
+          <Box
+            sx={{
+              display: 'flex',
+              alignItems: 'center',
+              flexDirection: 'column',
+              '& svg': {
+                mb: 8,
+                color: userInput === 'yes' ? 'success.main' : 'error.main'
+              }
+            }}
+          >
+            <Icon
+              fontSize='5.5rem'
+              icon={userInput === 'yes' ? 'mdi:check-circle-outline' : 'mdi:close-circle-outline'}
+            />
+            <Typography variant='h4' sx={{ mb: 5 }}>
+              {userInput === 'yes' ? 'Cuenta Eliminada' : 'Cancelada'}
+            </Typography>
+            <Typography>
+              {userInput === 'yes' ? 'Tu cuenta se ha eliminado correctamente.' : 'Eliminación de cuenta cancelada.'}
+            </Typography>
+          </Box>
+        </DialogContent>
+        <DialogActions
+          sx={{
+            justifyContent: 'center',
+            px: theme => [`${theme.spacing(5)} !important`, `${theme.spacing(15)} !important`],
+            pb: theme => [`${theme.spacing(8)} !important`, `${theme.spacing(12.5)} !important`]
+          }}
+        >
+          <Button variant='contained' color='success' onClick={handleSecondDialogClose}>
+            OK
+          </Button>
+        </DialogActions>
+      </Dialog>
+
     </Grid>
   )
 }
--- src/pages/plans/newPlan/index.tsx
+++ src/pages/plans/newPlan/index.tsx
@@ -1,27 +1,242 @@
+import React, { useState } from 'react';
+import Grid from '@mui/material/Grid';
+import Card from '@mui/material/Card';
+import CardHeader from '@mui/material/CardHeader';
+import CardContent from '@mui/material/CardContent';
+import Paper from '@mui/material/Paper';
+import Table from '@mui/material/Table';
+import { styled } from '@mui/material/styles';
+import TableHead from '@mui/material/TableHead';
+import TableBody from '@mui/material/TableBody';
+import TableContainer from '@mui/material/TableContainer';
+import TableRow, { TableRowProps } from '@mui/material/TableRow';
+import TableCell, { TableCellProps, tableCellClasses } from '@mui/material/TableCell';
+import Button, { ButtonProps } from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
 
-// ** MUI Imports
-import Grid from '@mui/material/Grid'
-import Card from '@mui/material/Card'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+const StyledTableCell = styled(TableCell)<TableCellProps>(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    color: theme.palette.common.white,
+    backgroundColor: theme.palette.common.black,
+  },
+  [`&.${tableCellClasses.body}`]: {
+    fontSize: 14,
+  },
+}));
+
+const ButtonStyled = styled(Button)<ButtonProps & { component?: React.ElementType; htmlFor?: string }>(
+  ({ theme }) => ({
+    [theme.breakpoints.down('sm')]: {
+      width: '100%',
+      textAlign: 'center',
+    },
+  })
+);
+
+const StyledTableRow = styled(TableRow)<TableRowProps>(({ theme }) => ({
+  '&:nth-of-type(odd)': {
+    backgroundColor: theme.palette.action.hover,
+  },
+  '&:last-of-type td, &:last-of-type th': {
+    border: 0,
+  },
+}));
+
+const createData = (nombre: string, series: number, repeticiones: number, peso: number) => {
+  return { nombre, series, repeticiones, peso };
+};
+const rows = [
+  createData('Press de banca plano', 4, 12, 80),
+  createData('Press de banca inclinado', 3, 12, 60),
+  createData('Press de banca declinado', 3, 12, 80),
+  createData('Cruce de poleas', 3, 12, 25),
+  createData('Remo con barra', 4, 12, 70),
+];
 
 const newPlan = () => {
+  const [planList, setPlanList] = useState(rows);
+  const [editingRow, setEditingRow] = useState(-1);
+  const [nombre, setNombre] = useState('');
+  const [series, setSeries] = useState(0);
+  const [repeticiones, setRepeticiones] = useState(0);
+  const [peso, setPeso] = useState(0);
+
+
+  const handleAddRow = () => {
+    setEditingRow(planList.length); // Establece el índice de edición en la longitud actual de la lista
+    setNombre('');
+    setSeries(0);
+    setRepeticiones(0);
+    setPeso(0);
+  };
+
+
+  const handleEditRow = (rowIndex: number) => {
+    setEditingRow(rowIndex);
+  };
+
+  const handleSaveRow = () => {
+    if (nombre.trim() !== '' && series > 0 && repeticiones > 0 && peso > 0) {
+      const updatedPlanList = [...planList];
+      updatedPlanList[editingRow] = createData(
+        nombre,
+        series,
+        repeticiones,
+        peso
+      );
+      setPlanList(updatedPlanList);
+      setEditingRow(null);
+      setNombre('');
+      setSeries(0);
+      setRepeticiones(0);
+      setPeso(0);
+    }
+  };
+
+
+  const handleCancelEditRow = () => {
+    setEditingRow(null);
+    setNombre('');
+    setSeries(0);
+    setRepeticiones(0);
+    setPeso(0);
+  };
+
+  const handleDeleteRow = (rowIndex: number) => {
+    const updatedPlanList = [...planList];
+    updatedPlanList.splice(rowIndex, 1);
+    setPlanList(updatedPlanList);
+  };
+
+
+
   return (
-    <Grid container spacing={6}>
-      <Grid item md={6} xs={12}>
-        <Card>
-          <CardHeader title='Nuevo Plan' />
-          <CardContent>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
-  )
-}
+    <TableContainer component={Paper}>
+      <Card>
+        <CardHeader title='Planes' />
+        <Table sx={{ minWidth: 700 }} aria-label='customized table'>
+          <TableHead>
+            <TableRow>
+              <StyledTableCell>Ejercicio</StyledTableCell>
+              <StyledTableCell align='right'>Series</StyledTableCell>
+              <StyledTableCell align='right'>Repeticiones</StyledTableCell>
+              <StyledTableCell align='right'>Peso</StyledTableCell>
+              <StyledTableCell align='right'>Acciones</StyledTableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {planList.map((row, index) => (
+              <StyledTableRow key={index}>
+                <StyledTableCell component='th' scope='row'>
+                  {editingRow === index ? (
+                    <TextField
+                      value={nombre}
+                      onChange={(e) => setNombre(e.target.value)}
+                    />
+                  ) : (
+                    row.nombre
+                  )}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {editingRow === index ? (
+                    <TextField
+                      type='number'
+                      value={series}
+                      onChange={(e) => setSeries(Number(e.target.value))}
+                    />
+                  ) : (
+                    row.series
+                  )}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {editingRow === index ? (
+                    <TextField
+                      type='number'
+                      value={repeticiones}
+                      onChange={(e) => setRepeticiones(Number(e.target.value))}
+                    />
+                  ) : (
+                    row.repeticiones
+                  )}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {editingRow === index ? (
+                    <TextField
+                      type='number'
+                      value={peso}
+                      onChange={(e) => setPeso(Number(e.target.value))}
+                    />
+                  ) : (
+                    row.peso
+                  )}
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  {editingRow === index ? (
+                    <>
+                      <ButtonStyled onClick={handleSaveRow}>Guardar</ButtonStyled>
+                      <ButtonStyled onClick={handleCancelEditRow}>Cancelar</ButtonStyled>
+                    </>
+                  ) : (
+                    <>
+                      <ButtonStyled onClick={() => handleEditRow(index)}>Editar</ButtonStyled>
+                      <ButtonStyled onClick={() => handleDeleteRow(index)}>Eliminar</ButtonStyled>
+                    </>
+                  )}
+                </StyledTableCell>
+              </StyledTableRow>
+            ))}
+            {editingRow === planList.length && (
+              <StyledTableRow>
+                <StyledTableCell component='th' scope='row'>
+                  <TextField
+                    value={nombre}
+                    onChange={(e) => setNombre(e.target.value)}
+                  />
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  <TextField
+                    type='number'
+                    value={series}
+                    onChange={(e) => setSeries(Number(e.target.value))}
+                  />
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  <TextField
+                    type='number'
+                    value={repeticiones}
+                    onChange={(e) => setRepeticiones(Number(e.target.value))}
+                  />
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  <TextField
+                    type='number'
+                    value={peso}
+                    onChange={(e) => setPeso(Number(e.target.value))}
+                  />
+                </StyledTableCell>
+                <StyledTableCell align='right'>
+                  <ButtonStyled onClick={handleSaveRow}>Guardar</ButtonStyled>
+                  <ButtonStyled onClick={handleCancelEditRow}>Cancelar</ButtonStyled>
+                </StyledTableCell>
+              </StyledTableRow>
+            )}
+          </TableBody>
+        </Table>
+        <CardContent>
+          <ButtonStyled variant='contained' onClick={handleAddRow}>
+            Agregar Ejercicio
+          </ButtonStyled>
+        </CardContent>
+      </Card>
+    </TableContainer >
+  );
+};
 
 newPlan.acl = {
   action: 'manage',
-  subject: 'newPlan-page'
-}
+  subject: 'newPlan-page',
+};
 
-export default newPlan
+export default newPlan;
+
+
