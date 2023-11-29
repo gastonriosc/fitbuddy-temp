ee562b85617c402615eeec3376c89607be90b5db
jmanero <ingmanerojoaquin@gmail.com>
2023-11-29 16:49:59 -0300

    cambio final en link

--- src/pages/plans/foro.tsx
+++ src/pages/plans/foro.tsx
@@ -276,6 +276,7 @@ const Foro = (props: Props) => {
                     )}
                   </Box>
 
+
                   <div style={{ flex: 1, textAlign: 'right' }}>
                     {message ? (
                       <>
