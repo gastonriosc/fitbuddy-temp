4a5460e0f35ebc240b7fba23af8dc12b97970f35
jmanero <ingmanerojoaquin@gmail.com>
2023-12-14 16:26:21 -0300

    cambio olvidado

--- src/pages/myRequests/[id].tsx
+++ src/pages/myRequests/[id].tsx
@@ -2,7 +2,7 @@
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 
-//import { useSession } from 'next-auth/react';
+
 
 // ** MUI Imports
 import Pagination from '@mui/material/Pagination'
