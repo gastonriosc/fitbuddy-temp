787b9c7b8a26f9f41fe167c69a91a74aabc4ad62
Gaston Rios Cardona <gastonriosc@gmail.com>
2023-12-14 19:46:02 -0300

    comment console.log(trainers)

--- src/pages/api/trainerSearch/index.ts
+++ src/pages/api/trainerSearch/index.ts
@@ -66,8 +66,7 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       }
     ])
     if (trainers && trainers.length > 0) {
-      console.log(trainers)
-
+      // console.log(trainers)
       return res.status(200).json(trainers)
     } else {
       return res.status(404).json({ message: 'No se encontraron usuarios' })
